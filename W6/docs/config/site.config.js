2e82ae4d39c180d3cf428b26d846da88d4b95fc9
Putranto13 <31311599+Putranto13@users.noreply.github.com>
2022-04-22 19:37:25 +0700

    Add files via upload

---
+++ W6/docs/config/site.config.js
@@ -0,0 +1,18 @@
+const { cdns, customScripts } = require("./script.config");
+
+module.exports = {
+    title: "Casper",
+    tagline: "The best search experience for docs, integrated in minutes, for free",
+    url: "https://operators.casper.network/",
+    baseUrl: "/docs/",
+    trailingSlash: true,
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "throw",
+    onDuplicateRoutes: "throw",
+    favicon: "image/favicon.ico",
+    organizationName: "CasperNetwork",
+    projectName: "Casper",
+    /* Optional */
+    // clientModules: customScripts,
+    // scripts: cdns,
+};
